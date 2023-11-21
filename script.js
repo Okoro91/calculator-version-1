2e1173378b82f33e55f960494b3ea5258c1d4910
okoro <mi.fracri91@gmail.com>
2023-11-21 06:01:27 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('input[name="display"]');
+const clear = document.querySelector('#ac');
+const equals = document.querySelector('#equal');
+const deleting = document.querySelector('#de');
+
+
+
+function outPutValue(output){
+    display.value += output;
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        outPutValue(button.value);
+    });
+});
+
+
+
+equals.addEventListener('click', () => {
+    let show = eval(display.value);
+    outPutValue(` = ${show}`);
+});
+
+deleting.addEventListener('click', () => {
+    let show = display.value
+    let result = show.split('').slice(0, -1).join('');
+    display.value = result;
+});
+
+clear.addEventListener('click', () => {
+    display.value = '';
+});
+
